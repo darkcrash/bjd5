89d2f6ef218b7396c81cb977836f5d49a7e302c1
YASUAKI MATSUDA <dark_crash@hotmail.com>
2016-01-03 21:18:18 +0900

    repair

---
+++ Bjd.TftpServer.CoreCLR/Option.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Bjd;
+using Bjd.ctrl;
+using Bjd.net;
+using Bjd.option;
+
+namespace Bjd.TftpServer
+{
+    class Option : OneOption
+    {
+
+        //public override string JpMenu { get { return "TFTPサーバ"; } }
+        //public override string EnMenu { get { return "TFTP Server"; } }
+        public override char Mnemonic { get { return 'T'; } }
+
+        public Option(Kernel kernel, string path, string nameTag)
+            : base(kernel.IsJp(), path, nameTag)
+        {
+
+            //var key = "useServer";
+            //Add(new OneVal(key, false, Crlf.Nextline, new CtrlCheckBox(Lang.Value(key))));
+
+            //var pageList = new List<OnePage>();
+            //key = "Basic";
+            //pageList.Add(Page1(key, Lang.Value(key), kernel));
+            //pageList.Add(PageAcl());
+            //Add(new OneVal("tab", null, Crlf.Nextline, new CtrlTabPage("tabPage", pageList)));
+
+            Add(new OneVal("useServer", false, Crlf.Nextline));
+
+            var pageList = new List<OnePage>();
+            pageList.Add(Page1("Basic", Lang.Value("Basic"), kernel));
+            pageList.Add(PageAcl());
+            Add(new OneVal("tab", null, Crlf.Nextline));
+
+            Read(kernel.IniDb); //　レジストリからの読み込み
+        }
+
+        private OnePage Page1(string name, string title, Kernel kernel)
+        {
+            var onePage = new OnePage(name, title);
+            //onePage.Add(CreateServerOption(ProtocolKind.Udp, 69, 60, 10)); //サーバ基本設定
+            CreateServerOption(ProtocolKind.Udp, 69, 60, 10);
+
+            //var key = "workDir";
+            //onePage.Add(new OneVal(key, "", Crlf.Nextline, new CtrlFolder(Lang.Value(key), 60, kernel)));
+            //key = "read";
+            //onePage.Add(new OneVal(key, false, Crlf.Nextline, new CtrlCheckBox(Lang.Value(key))));
+            //key = "write";
+            //onePage.Add(new OneVal(key, false, Crlf.Nextline, new CtrlCheckBox(Lang.Value(key))));
+            //key = "override";
+            //onePage.Add(new OneVal(key, false, Crlf.Nextline, new CtrlCheckBox(Lang.Value(key))));
+
+            Add(new OneVal("workDir", "Tftp", Crlf.Nextline));
+            Add(new OneVal("read", false, Crlf.Nextline));
+            Add(new OneVal("write", false, Crlf.Nextline));
+            Add(new OneVal("override", false, Crlf.Nextline));
+
+            return onePage;
+        }
+
+    }
+}
+
