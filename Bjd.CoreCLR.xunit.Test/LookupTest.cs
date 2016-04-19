a8af06c5ce1a9a5a3d7aeada028b17a7f6ebd342
YASUAKI MATSUDA <dark_crash@hotmail.com>
2016-04-20 07:52:53 +0900

    add xunit project

--- Bjd.CoreCLR.xunit.Test/LookupTest.cs
+++ Bjd.CoreCLR.xunit.Test/LookupTest.cs
@@ -2,28 +2,21 @@
 using Xunit;
 using Bjd;
 
-namespace BjdTest {
-    
-    //[TestFixture(TestName = "LookupTest", Category = "Bjd.Common")]
-    public class LookupTestxunit {
-        
-        
-        //[SetUp]
-        public void SetUp() {
-        }
-        
-        //[TearDown]
-        public void TearDown() {
-        }
+namespace BjdTest
+{
+
+    public class LookupTestxunit
+    {
 
 
         [Fact]
-        public void DnsServerTest() {
+        public void DnsServerTest()
+        {
             var o = Lookup.DnsServer();
-            Assert.NotEqual(o.Count,0);
-            
+            Assert.NotEqual(o.Count, 0);
+
             //デフォルトゲートウエイ確認 ※環境の違いを吸収
-            if (o[0] != "192.168.0.254" && o[0] != "10.0.0.1" && o[0] != "192.168.1.1" && o[0]!="192.168.113.2" && o[0] != "192.168.9.26" && o[0] != "192.168.43.1")
+            if (o[0] != "192.168.0.254" && o[0] != "10.0.0.1" && o[0] != "192.168.1.1" && o[0] != "192.168.113.2" && o[0] != "192.168.9.26" && o[0] != "192.168.43.1")
             {
                 //Assert.Fail();
                 throw new System.Exception();
@@ -35,13 +28,15 @@ namespace BjdTest {
         [InlineData("www.sapporoworks.ne.jp", "59.106.27.208")]
         [InlineData("yahoo.co.jp", "182.22.59.229")]
         [InlineData("yahoo.co.jp", "183.79.135.206")]
-        public void QueryATest(string target, string ipStr) {
+        public void QueryATest(string target, string ipStr)
+        {
             var o = Lookup.QueryA(target);
-            Assert.NotEqual(o.Count,0);
-            if (o.Any(s => s == ipStr)){
+            Assert.NotEqual(o.Count, 0);
+            if (o.Any(s => s == ipStr))
+            {
                 return;
             }
-            Assert.Contains(ipStr,o);
+            Assert.Contains(ipStr, o);
         }
 
         [Theory]
@@ -51,13 +46,15 @@ namespace BjdTest {
         [InlineData("google.com", "alt3.aspmx.l.google.com.")]
         [InlineData("google.com", "alt4.aspmx.l.google.com.")]
         [InlineData("sapporoworks.ne.jp", "spw02.sakura.ne.jp.")]
-        public void QueryMxTest(string target,string answer) {
+        public void QueryMxTest(string target, string answer)
+        {
             var d = Lookup.DnsServer();
             var dnsServer = d[0];
-            var o = Lookup.QueryMx(target,dnsServer);
+            var o = Lookup.QueryMx(target, dnsServer);
             Assert.NotEqual(o.Count, 0);
 
-            if (o.Any(s => s == answer)){
+            if (o.Any(s => s == answer))
+            {
                 return;
             }
             Assert.Contains(answer, o);
---
+++ Bjd.CoreCLR.xunit.Test/MacTest.cs
@@ -0,0 +1,44 @@
+﻿using Bjd.net;
+using Xunit;
+using Bjd;
+
+namespace BjdTest {
+    
+    public class MacTest {
+        
+        //[SetUp]
+        public void SetUp() {
+        }
+        
+        //[TearDown]
+        public void TearDown() {
+        
+        }
+
+        [Theory]
+        [InlineData("00-00-00-00-00-00")]
+        [InlineData("FF-FF-FF-FF-FF-FF")]
+        [InlineData("00-26-2D-3F-3F-67")]
+        [InlineData("00-ff-ff-ff-3F-67")]
+        public void ToStringTest(string macStr) {
+            var target = new Mac(macStr);
+            Assert.Equal(target.ToString(), macStr.ToUpper());
+        }
+
+        [Theory]
+        [InlineData("00-00-00-00-00-00")]
+        [InlineData("FF-FF-FF-FF-FF-FF")]
+        [InlineData("00-26-2D-3F-3F-67")]
+        [InlineData("00-ff-ff-ff-3F-67")]
+        public void OperandTest(string macStr) {
+            const string dmy = "11-11-11-11-11-11";
+            Assert.Equal(new Mac(macStr) == new Mac(macStr), true);
+            Assert.Equal(new Mac(macStr) != new Mac(macStr), false);
+            Assert.Equal(new Mac(dmy) == new Mac(macStr), false);
+            Assert.Equal(new Mac(dmy) != new Mac(macStr), true);
+            Assert.Equal(new Mac(macStr) == null, false);
+            Assert.Equal(new Mac(macStr) != null, true);
+        }
+
+    }
+}
