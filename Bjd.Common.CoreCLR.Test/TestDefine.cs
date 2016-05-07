1749722a9c56e16b0501e7c6c602649afc10c00d
YASUAKI MATSUDA <dark_crash@hotmail.com>
2016-05-07 14:37:28 +0900

    revert option.ini

---
+++ Bjd.Common.CoreCLR.Test/TestDefine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bjd.Common.CoreCLR.Test
+{
+    public class TestDefine
+    {
+
+        public static readonly TestDefine Instance = new TestDefine();
+
+        public readonly string TestDirectory;
+        public readonly string TestMailboxPath;
+
+        private TestDefine()
+        {
+            TestDirectory = System.IO.Path.GetDirectoryName(AppContext.BaseDirectory);
+            TestDirectory = System.IO.Path.Combine(TestDirectory, "TestDirectory");
+            try
+            {
+                System.IO.Directory.Delete(TestDirectory, true);
+            }
+            catch { }
+
+            TestMailboxPath = System.IO.Path.Combine(TestDirectory, "mailbox");
+
+            System.IO.Directory.CreateDirectory(TestDirectory);
+            System.IO.Directory.CreateDirectory(TestMailboxPath);
+
+        }
+
+
+    }
+}
