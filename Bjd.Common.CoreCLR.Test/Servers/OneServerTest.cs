28e92f18e1b2dfc6ca930b36c95dd95ae8552a8f
yasuaki.matsuda <dark_crash@hotmail.com>
2016-07-16 17:03:11 +0900

    modify test server port

--- Bjd.Common.CoreCLR.Test/Servers/OneServerTest.cs
+++ Bjd.Common.CoreCLR.Test/Servers/OneServerTest.cs
@@ -140,7 +140,7 @@ namespace Bjd.Test.Servers
             var ip = new Ip(IpKind.V4Localhost);
             var oneBind = new OneBind(ip, ProtocolKind.Tcp);
             Conf conf = TestUtil.CreateConf("OptionSample");
-            conf.Set("port", 9980);
+            conf.Set("port", 9997);
             conf.Set("multiple", 10);
             conf.Set("acl", new Dat(new CtrlType[0]));
             conf.Set("enableAcl", 1);
